24fcbe468370c2fb9bd3321aef2983c87d6ad436
Rolly <rolly@stratiumsoftware.com>
2019-06-21 09:44:14 +0800

    Initial Commit

---
+++ BGXRetailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BGXRetailApp
+//
+//  Created by Rolly on 06/21/2019.
+//  Copyright © 2019 Beagleworx Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
